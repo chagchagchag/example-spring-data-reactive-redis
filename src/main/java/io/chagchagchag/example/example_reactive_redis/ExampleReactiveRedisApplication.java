abb55cbc9fe8181f1e4be9dee8999750887c868e
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-03 18:35:58 +0900

    first status

---
+++ src/main/java/io/chagchagchag/example/example_reactive_redis/ExampleReactiveRedisApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.example_reactive_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleReactiveRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleReactiveRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/chagchagchag/example/example_reactive_redis/ExampleReactiveRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.example_reactive_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleReactiveRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
